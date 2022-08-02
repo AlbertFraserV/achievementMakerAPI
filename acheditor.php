bc41b75d06343aad234277b45d84e21b4e894a43
AlbertFraserV <109490068+AlbertFraserV@users.noreply.github.com>
2022-08-02 18:44:18 -0400

    Update acheditor.php

--- acheditor.php
+++ acheditor.php
@@ -5,20 +5,9 @@ ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 require __DIR__ . "/inc/bootstrap.php";
 require PROJECT_ROOT_PATH . "/Controller/Api/AchEditorController.php"; 
- 
-/*
-Base url: https://www.blipgaming.xyz/api/acheditor.php
 
-Query Parameters:
 
-*query_type
-Valid entires: "game" or "memory"
-
-*game_key
-Valid entries: any positive integer
-*/
-
-if (!isset($_GET["game_key"]) || !isset($_GET["query_type"]))
+if (!isset($_GET["query_type"]))
 {
     $baseController = new BaseController();
     $baseController->send404();
